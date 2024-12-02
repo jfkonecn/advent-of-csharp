e6c8668ea402b143475ca2e3b0307731ebb8d321
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2024-12-02 07:36:01 -0500

    Day 2 Part 1

--- AdventOfCSharp.Console.Tests/Year2024/Day02/Solution202401Tests.cs
+++ AdventOfCSharp.Console.Tests/Year2024/Day02/Solution202401Tests.cs
@@ -5,7 +5,8 @@ namespace AdventOfCSharp.Console.Tests.Year2024.Day02;
 [TestFixture]
 public class Solution202402Tests
 {
-    [TestCase("example1.txt", 0)]
+    [TestCase("example1.txt", 2)]
+    [TestCase("real.txt", 402)]
     public async Task Solution1Tests(string fileName, int expected)
     {
         var fileContents = await Utils.GetAdventTestFile(2024, 2, fileName);
@@ -13,7 +14,7 @@ public class Solution202402Tests
         Assert.That(actual, Is.EqualTo(expected));
     }
 
-    [TestCase("example1.txt", 0)]
+    //[TestCase("example1.txt", 0)]
     public async Task Solution2Tests(string fileName, int expected)
     {
         var fileContents = await Utils.GetAdventTestFile(2024, 2, fileName);
--- AdventOfCSharp.Console/Year2024/Day02/Solution202402.cs
+++ AdventOfCSharp.Console/Year2024/Day02/Solution202402.cs
@@ -2,9 +2,34 @@ namespace AdventOfCSharp.Console.Year2024.Day02;
 
 public static class Solution202402
 {
+    private static int[][] Parse(string[] fileContents)
+    {
+        return fileContents
+            .Select(line =>
+            {
+                return line.Split(' ').Select(int.Parse).ToArray();
+            })
+            .ToArray();
+    }
+
     public static int Solution1(string[] fileContents)
     {
-        throw new NotImplementedException();
+        return Parse(fileContents)
+            .Where(arr =>
+            {
+                var isIncreasing = arr[1] - arr[0] >= 0;
+                for (int i = 1; i < arr.Length; i++)
+                {
+                    var diff = arr[i] - arr[i - 1];
+                    var curIsIncreasing = diff >= 0;
+                    if (isIncreasing != curIsIncreasing || Math.Abs(diff) > 3 || diff == 0)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            })
+            .Count();
     }
 
     public static int Solution2(string[] fileContents)
