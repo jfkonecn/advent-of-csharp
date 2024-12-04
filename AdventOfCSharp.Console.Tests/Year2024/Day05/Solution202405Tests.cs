8cf8295e0cbf405cb4983f513855e64a1d9e2451
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2024-12-04 07:50:08 -0500

    Day 5

---
+++ AdventOfCSharp.Console.Tests/Year2024/Day05/Solution202405Tests.cs
@@ -0,0 +1,23 @@
+using AdventOfCSharp.Console.Year2024.Day05;
+
+namespace AdventOfCSharp.Console.Tests.Year2024.Day05;
+
+[TestFixture]
+public class Solution202405Tests
+{
+    [TestCase("example1.txt", 0)]
+    public async Task Solution1Tests(string fileName, int expected)
+    {
+        var fileContents = await Utils.GetAdventTestFile(2024, 05, fileName);
+        var actual = Solution202405.Solution1(fileContents);
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [TestCase("example1.txt", 0)]
+    public async Task Solution2Tests(string fileName, int expected)
+    {
+        var fileContents = await Utils.GetAdventTestFile(2024, 05, fileName);
+        var actual = Solution202405.Solution2(fileContents);
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}
---
+++ AdventOfCSharp.Console/Year2024/Day05/Solution202405.cs
@@ -0,0 +1,14 @@
+namespace AdventOfCSharp.Console.Year2024.Day05;
+
+public static class Solution202405
+{
+    public static int Solution1(string[] fileContents)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static int Solution2(string[] fileContents)
+    {
+        throw new NotImplementedException();
+    }
+}
