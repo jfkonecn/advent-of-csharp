c5f53b14e355969ae914d361fffe28976079465e
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2024-12-06 08:12:52 -0500

    Day 6 Part 2

--- AdventOfCSharp.Console.Tests/Year2024/Day06/Solution202406Tests.cs
+++ AdventOfCSharp.Console.Tests/Year2024/Day06/Solution202406Tests.cs
@@ -14,9 +14,11 @@ public class Solution202406Tests
         Assert.That(actual, Is.EqualTo(expected));
     }
 
-    [TestCase("example1.txt", 0)]
+    [TestCase("example1.txt", 6)]
+    [TestCase("real.txt", 2124)]
     public async Task Solution2Tests(string fileName, int expected)
     {
+        // 2124 is too high
         var fileContents = await Utils.GetAdventTestFile(2024, 06, fileName);
         var actual = Solution202406.Solution2(fileContents);
         Assert.That(actual, Is.EqualTo(expected));
--- AdventOfCSharp.Console/Year2024/Day06/Solution202406.cs
+++ AdventOfCSharp.Console/Year2024/Day06/Solution202406.cs
@@ -116,6 +116,144 @@ public static class Solution202406
 
     public static int Solution2(string[] fileContents)
     {
-        throw new NotImplementedException();
+        var (points, (i, j)) = Parse(fileContents);
+        var direction = Direction.Up;
+        int blocks = 0;
+        while (points.GetLength(0) > i && points.GetLength(1) > j && i >= 0 && j >= 0)
+        {
+            if (direction == Direction.Up && i > 0 && points[i - 1, j] == Point.Occupied)
+            {
+                direction = Direction.Right;
+            }
+            else if (
+                direction == Direction.Right
+                && points.GetLength(1) > j + 1
+                && points[i, j + 1] == Point.Occupied
+            )
+            {
+                direction = Direction.Down;
+            }
+            else if (
+                direction == Direction.Down
+                && points.GetLength(0) > i + 1
+                && points[i + 1, j] == Point.Occupied
+            )
+            {
+                direction = Direction.Left;
+            }
+            else if (direction == Direction.Left && j > 0 && points[i, j - 1] == Point.Occupied)
+            {
+                direction = Direction.Up;
+            }
+            else if (direction == Direction.Up)
+            {
+                if (DoesLoop(points, i, j, direction))
+                {
+                    blocks++;
+                }
+
+                i--;
+            }
+            else if (direction == Direction.Right)
+            {
+                if (DoesLoop(points, i, j, direction))
+                {
+                    blocks++;
+                }
+
+                j++;
+            }
+            else if (direction == Direction.Down)
+            {
+                if (DoesLoop(points, i, j, direction))
+                {
+                    blocks++;
+                }
+
+                i++;
+            }
+            else if (direction == Direction.Left)
+            {
+                if (DoesLoop(points, i, j, direction))
+                {
+                    blocks++;
+                }
+
+                j--;
+            }
+        }
+        return blocks;
+    }
+
+    private static bool DoesLoop(Point[,] points, int i, int j, Direction direction)
+    {
+        if (
+            (i == 0 && direction == Direction.Up)
+            || (j == 0 && direction == Direction.Left)
+            || (i + 1 == points.GetLength(0) && direction == Direction.Down)
+            || (j + 1 == points.GetLength(1) && direction == Direction.Right)
+        )
+        {
+            return false;
+        }
+        var visited = new HashSet<(int i, int j, Direction direction)>() { (i, j, direction) };
+
+        direction = direction switch
+        {
+            Direction.Up => Direction.Right,
+            Direction.Right => Direction.Down,
+            Direction.Down => Direction.Left,
+            Direction.Left => Direction.Up,
+            _ => throw new Exception($"Unknown direction \"{direction.ToString()}\""),
+        };
+        while (points.GetLength(0) > i && points.GetLength(1) > j && i >= 0 && j >= 0)
+        {
+            if (visited.Contains((i, j, direction)))
+            {
+                return true;
+            }
+            visited.Add((i, j, direction));
+            if (direction == Direction.Up && i > 0 && points[i - 1, j] == Point.Occupied)
+            {
+                direction = Direction.Right;
+            }
+            else if (
+                direction == Direction.Right
+                && points.GetLength(1) > j + 1
+                && points[i, j + 1] == Point.Occupied
+            )
+            {
+                direction = Direction.Down;
+            }
+            else if (
+                direction == Direction.Down
+                && points.GetLength(0) > i + 1
+                && points[i + 1, j] == Point.Occupied
+            )
+            {
+                direction = Direction.Left;
+            }
+            else if (direction == Direction.Left && j > 0 && points[i, j - 1] == Point.Occupied)
+            {
+                direction = Direction.Up;
+            }
+            else if (direction == Direction.Up)
+            {
+                i--;
+            }
+            else if (direction == Direction.Right)
+            {
+                j++;
+            }
+            else if (direction == Direction.Down)
+            {
+                i++;
+            }
+            else if (direction == Direction.Left)
+            {
+                j--;
+            }
+        }
+        return false;
     }
 }
