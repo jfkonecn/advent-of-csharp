5f52f1afadfa2f8b0240b99b447e9a0303f31972
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2024-12-13 07:05:53 -0500

    Day 13

---
+++ AdventOfCSharp.Console.Tests/Year2024/Day13/Solution202413Tests.cs
@@ -0,0 +1,23 @@
+using AdventOfCSharp.Console.Year2024.Day13;
+
+namespace AdventOfCSharp.Console.Tests.Year2024.Day13;
+
+[TestFixture]
+public class Solution202413Tests
+{
+    [TestCase("example1.txt", 0)]
+    public async Task Solution1Tests(string fileName, int expected)
+    {
+        var fileContents = await Utils.GetAdventTestFile(2024, 13, fileName);
+        var actual = Solution202413.Solution1(fileContents);
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [TestCase("example1.txt", 0)]
+    public async Task Solution2Tests(string fileName, int expected)
+    {
+        var fileContents = await Utils.GetAdventTestFile(2024, 13, fileName);
+        var actual = Solution202413.Solution2(fileContents);
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}
