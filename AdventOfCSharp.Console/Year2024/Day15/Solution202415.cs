dce17bf0fafe4ec2ce090d7f60e3b66016859494
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2024-12-15 09:38:55 -0500

    Day 15 Part 2

--- AdventOfCSharp.Console/Year2024/Day15/Solution202415.cs
+++ AdventOfCSharp.Console/Year2024/Day15/Solution202415.cs
@@ -246,79 +246,79 @@ public static class Solution202415
             }
         }
         Print(Move.Unknown);
-        //foreach (var move in result.Moves)
-        //{
-        //var dy = move switch
-        //{
-        //Move.Up => -1,
-        //Move.Down => 1,
-        //_ => 0,
-        //};
-        //var dx = move switch
-        //{
-        //Move.Left => -1,
-        //Move.Right => 1,
-        //_ => 0,
-        //};
+        foreach (var move in result.Moves)
+        {
+            var dy = move switch
+            {
+                Move.Up => -1,
+                Move.Down => 1,
+                _ => 0,
+            };
+            var dx = move switch
+            {
+                Move.Left => -1,
+                Move.Right => 1,
+                _ => 0,
+            };
 
-        //bool canMove = true;
+            bool canMove = true;
 
-        //var tempY = robotY + dy;
-        //var tempX = robotX + dx;
-        //int boxesToPush = 0;
+            var tempY = robotY + dy;
+            var tempX = robotX + dx;
+            int boxesToPush = 0;
 
-        //while (warehouse[tempY, tempX] != Item.Empty)
-        //{
-        //var item = warehouse[tempY, tempX];
-        //if (item == Item.Wall)
-        //{
-        //canMove = false;
-        //break;
-        //}
-        //else if (item == Item.Box)
-        //{
-        //tempY += dy;
-        //tempX += dx;
-        //boxesToPush++;
-        //}
-        //else
-        //{
-        //throw new Exception($"{item} should be either a Box or Wall");
-        //}
-        //}
+            while (warehouse[tempY, tempX] != Item.Empty)
+            {
+                var item = warehouse[tempY, tempX];
+                if (item == Item.Wall)
+                {
+                    canMove = false;
+                    break;
+                }
+                else if (item == Item.Box)
+                {
+                    tempY += dy;
+                    tempX += dx;
+                    boxesToPush++;
+                }
+                else
+                {
+                    throw new Exception($"{item} should be either a Box or Wall");
+                }
+            }
 
-        //if (canMove)
-        //{
-        //if (boxesToPush == 2)
-        //{
-        //_ = 1;
-        //}
-        //robotY += dy;
-        //robotX += dx;
+            if (canMove)
+            {
+                if (boxesToPush == 2)
+                {
+                    _ = 1;
+                }
+                robotY += dy;
+                robotX += dx;
 
-        //warehouse[robotY, robotX] = Item.Empty;
+                warehouse[robotY, robotX] = Item.Empty;
 
-        //var y = robotY + dy;
-        //var x = robotX + dx;
+                var y = robotY + dy;
+                var x = robotX + dx;
 
-        //for (int i = 0; i < boxesToPush; i++)
-        //{
-        //warehouse[y, x] = Item.Box;
-        //y += dy;
-        //x += dx;
-        //}
-        //}
-        //Print(move);
-        //}
+                for (int i = 0; i < boxesToPush; i++)
+                {
+                    warehouse[y, x] = Item.Box;
+                    y += dy;
+                    x += dx;
+                }
+            }
+            Print(move);
+        }
 
         long sum = 0;
-        for (long x = 0; x < warehouse.GetLength(0); x++)
+        for (long y = 0; y < warehouse.GetLength(0); y++)
         {
-            for (long y = 0; y < warehouse.GetLength(1); y++)
+            for (long x = 0; x < warehouse.GetLength(1); x++)
             {
-                if (warehouse[x, y] == Item.Box)
+                if (warehouse[y, x] == Item.Box)
                 {
-                    sum += (100 * x) + y;
+                    sum += (100 * y) + x;
                 }
             }
         }
